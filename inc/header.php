672dea314c084cda39ba93af1d9d8e0be9ae132d
PrestonMclean <mclean_preston@hotmail.com>
2021-03-02 18:11:44 -0600

    header
    remove the header and put it in a separate file

---
+++ inc/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Phrase Hunter</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="css/styles.css" rel="stylesheet">
+		<link href="css/animate.css" rel="stylesheet">
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
+	</head>
+
+	<body>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once('inc/header.php');
+?>
+		<div class="main-container">
+			<h2 class="header">Phrase Hunter</h2>
+      <form action="play.php" method="post">
+        <input id="phraseInput" type="text" name="phrase" value="">
+        <input id="btn__reset" type="submit" name="start" value="Start Game" />
+      </form>
+		</div>
+	</body>
+</html>
